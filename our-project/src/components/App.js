3b6fd9c568c0e6a5b153cc89fb93b070fcf84b61
Chun Yi <8.5by11@gmail.com>
2022-02-14 16:38:30 -0500

    Dunkin color nav

--- our-project/src/components/App.js
+++ our-project/src/components/App.js
@@ -19,6 +19,7 @@ function App() {
         <Route path="/interact">
           <Interact />
         </Route>
+        
       </Switch>
     </div>  
   );
--- our-project/src/components/NavBar.js
+++ our-project/src/components/NavBar.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 function NavBar() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
+      <NavLink exact to="/">Home</NavLink>
       <NavLink to="/coffee">Coffee</NavLink>
       <NavLink to="/interact">Interact</NavLink>
     </nav>
