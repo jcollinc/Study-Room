b49fdcb7201aae4c0d60c1c3f9a3cf739ddb6f15
Chun Yi <8.5by11@gmail.com>
2022-02-14 13:03:40 -0500

    prepping routes

---
+++ our-project/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavBar from "./NavBar"
+import DataCards from "./DataCards"
+
+function App() {
+  return (
+    <Route path="/">
+      <Home />
+    </Route>
+
+  );
+}
+
+export default App;
