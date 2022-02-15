f1e330f6ddbcf456acc08d2e9cb2790ac80f55bb
Chun Yi <8.5by11@gmail.com>
2022-02-15 00:07:24 -0500

    Added fName key and hardcoded Cohort names

--- our-project/src/components/Home.js
+++ our-project/src/components/Home.js
@@ -3,10 +3,16 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <ul>
+      <a href={"https://learning.flatironschool.com"}>⬅️ Back to SENG-LIVE-011022-P2 Dashboard</a>
+
+      <h3>Introducing Remote Coffee Breaks Hosted by The Wedge School</h3>
+
+      <h4>Available for a limited time only, to celebrate Project Week we have partnered with Seattle's finest coffee purveyor to host caffeine-fueled stand-ups/stand-downs to our remote cohorts spread out across America!</h4>
+
+      <img src="https://m.psecn.photoshelter.com/img-get/I0000EQLxdBqaqh8/s/1000?1644911951"/>
 
 
-      </ul>
+
     </div>
   )
 }
