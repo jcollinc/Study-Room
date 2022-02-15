7f911271bebb8bc6ab5c0d2d3c130453d3820b4b
Chun Yi <8.5by11@gmail.com>
2022-02-15 13:41:16 -0500

    - coffee.json: added cohorts data - Interact.js: fetch GET, mapped cohort to page

--- our-project/src/components/Interact.js
+++ our-project/src/components/Interact.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Interact() {
 
+	const [cohorts, setCohorts] = useState([])
+
+	useEffect(() => {
+		fetch('http://localhost:3000/cohorts')
+		.then(res => res.json())
+		.then(cohorts => {
+			console.log('hi from interact', cohorts)
+			setCohorts(cohorts)
+		})
+
+	}, [])
+
+	let itemsToDisplay = cohorts
+		.map(cohort => {
+			return (
+				<ul key={cohort.fName}>
+					{cohort.fName}, {cohort.title}
+					<img src={cohort.image}/>
+				</ul>
+				
+			)
+		})
+
 
 	return (
-		<div>Interact</div>
+		<>{itemsToDisplay}</>
 	)
 }
 
