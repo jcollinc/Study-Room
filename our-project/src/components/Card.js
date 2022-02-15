5ab3233e65ce69ae68571b6e304f3e300d99ce41
Chun Yi <8.5by11@gmail.com>
2022-02-15 00:07:49 -0500

    Added fName to Cards

--- our-project/src/components/Card.js
+++ our-project/src/components/Card.js
@@ -2,24 +2,26 @@ import React from 'react'
 
 function Card({ Card, coffee }) {
 
-    const { image, title, description } = coffee
+    const { fName, image, title, description } = coffee
 
-  return (
-    <li className="card">
-        <div className="image">
-            <img
-                src={image}
-                alt={title}
-            />
-        </div>
-        <div>
-            <div className="card_content">
-                <div className="card_title">{title}</div>
-                <p className="card_text">{description}</p>
+    return (
+        <li className="card">
+            <div className="image">
+                <img
+                    src={image}
+                    alt={title}
+                />
             </div>
-        </div>
-    </li>
-  )
+            <div>    
+                <div className="card_content">
+                    <div className="card_name">{fName}</div>
+                    <div className="card_title">{title}</div>
+                    <p className="card_text">{description}</p>
+                </div>
+                <button>Add to Cart</button>
+            </div>
+        </li>
+    )
 }
 
 export default Card
