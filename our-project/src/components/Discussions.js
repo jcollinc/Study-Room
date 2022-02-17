c083b81f1f02c510a6fc5c3d58cff219ccfe718b
Chun Yi <8.5by11@gmail.com>
2022-02-17 12:19:03 -0500

    wip

--- our-project/src/components/Discussions.js
+++ our-project/src/components/Discussions.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Discussions({ handleAddItem }) {
 
 	const [comment, setComment] = useState('')
+	const [name, setName] = useState('')
 	const [error, setError] = useState('')
+	const [suggestions, setSuggestions] = useState([])
 
     function handleSubmit(e) {
       e.preventDefault();
@@ -30,31 +32,64 @@ function Discussions({ handleAddItem }) {
       }
     }
 
-	return (
+	useEffect(() => {
+		fetch('http://localhost:3000/suggestions')
+		.then(res => res.json())
+		.then(suggestions => {
+			setSuggestions(suggestions)
+		})
+	}, [])
+
+	let itemsToDisplay = suggestions.map(suggestion => <p key={suggestion.id}>{suggestion.name} {suggestion.comment} </p>)
+
+	let form = (
 		<div className="form-container">
-			<form onSubmit={handleSubmit}>
+			<form className="form" onSubmit={handleSubmit}  >
 				<label htmlFor='Form'>Let's Discuss!</label>
-				<input
-				type='text'
-				id='fName'
-				value={comment}
-				placeholder="Share your thoughts..."
-				onChange={e => setComment(e.target.value)}
-				/>		
-				<button type="submit">Submit</button>
+				<textarea
+					className="name"
+					type='text'
+					id='name'
+					value={name}
+					placeholder="Your name..."
+					onChange={e => setName(e.target.value)}
+				/>
+				<textarea
+					className="comments"
+					type='text'
+					id='id'
+					value={comment}
+					placeholder="Share your thoughts..."
+					onChange={e => setComment(e.target.value)}
+				/>
+
+				<button className="card_button" type="submit">Submit</button>
+			
 				
 				{/* And the error state */}
 				{
-				error.length > 0
-					? error.map((error, index) => (
-					<p key={index} style={{ color: "red" }}>
-						{error}
-					</p>
-					))
-					: null
+					error.length > 0
+						? error.map((error, index) => (
+						<p key={index} style={{ color: "red" }}>
+							{error}
+						</p>
+						))
+						: null
 				}
 			</form> 
+			
 		</div>
+
+	)
+
+	return (
+		<div className="discussion">
+			{form}
+			<div className="comments-container">
+				<h3>Comments</h3>
+				{itemsToDisplay}
+			</div>		
+		</div>	
 	)
 }
 
